511c5bfc19b4a64d9fb15dfbd518451f4827756c
BINAYA THAPA MAGAR <binayathapamagar@BINAYAs-MacBook-Pro.local>
2024-07-12 01:27:47 -0400

    Setup firebase storeage

---
+++ Group10NatureWalkSession/Controllers/FireStorage.swift
@@ -0,0 +1,19 @@
+//
+//  FireStorage.swift
+//  Group10NatureWalkSession
+//
+//  Created by BINAYA THAPA MAGAR on 2024-07-12.
+//
+
+import Foundation
+import FirebaseStorage
+
+class FireStorage: ObservableObject {
+    
+    // MARK: Static properties
+    
+
+    // MARK: Static methods
+    
+    
+}
